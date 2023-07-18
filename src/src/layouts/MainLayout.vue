47b9aa65b0a48018e0c9b46246a21bb91b59f4bf
Tino Hager <tino.hager@nager.at>
2023-07-18 21:34:18 +0200

    Initial commit

---
+++ src/src/layouts/MainLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header
+      elevated
+      class="bg-black"
+    >
+      <q-toolbar>
+        <q-toolbar-title>
+          Vlbg MAP
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
---
+++ src/src/pages/IndexPage.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import MapView from 'components/MapView.vue'
+
+const selectedCity = ref('')
+
+function cityChanged (name : string) {
+  selectedCity.value = name
+}
+</script>
+
+<template>
+  <q-page class="row">
+    <div class="col-6">
+      <MapView @selectionchanged="cityChanged" />
+    </div>
+    <div class="col-6">
+      <h1>{{ selectedCity }}</h1>
+    </div>
+  </q-page>
+</template>
