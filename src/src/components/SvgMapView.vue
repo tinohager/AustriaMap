e2d4a4e499d841cc6833c4eb40ff7fac9d6926ec
Tino Hager <tino.hager@nager.at>
2024-06-08 14:09:09 +0200

    Update SvgMapView.vue

--- src/src/components/SvgMapView.vue
+++ src/src/components/SvgMapView.vue
@@ -8,8 +8,6 @@ import { MapDataProvider } from 'src/models/MapDataProvider'
 
 const emit = defineEmits(['selectionchanged', 'zoomchanged'])
 
-const debugRef = ref(true)
-
 export interface Props {
   mapDataProvider: MapDataProvider
   zoom?: number
@@ -20,19 +18,12 @@ const props = defineProps<Props>()
 const selected = ref<string | undefined>(undefined)
 const hover = ref<string | undefined>(undefined)
 const svgRef = ref<SVGSVGElement>()
-const point1 = ref()
-const point2 = ref({ x: 0, y: 0 })
+const isMouseDown = ref(false)
 
-const pointCircle1 = ref({ x: 0, y: 0 })
 const pointCircle2 = ref({ x: 0, y: 0 })
 
-const ZOOM_SPEED = 0.2
 const tempZoom = ref(props.zoom || 1)
 
-const mousePosition = ref({
-  x: 0,
-  y: 0
-})
 const currentPosition = ref({
   x: 0,
   y: 0
@@ -41,9 +32,6 @@ const startPosition = ref({
   x: 0,
   y: 0
 })
-
-const appendedCircle = false
-const isMouseDown = ref(false)
 
 function createPolygon (points: MapItemPoint[]) {
   if (!points.length) {
@@ -76,6 +64,10 @@ function selectItem (name: string) {
 }
 
 function isHover (item: MapItem) {
+  if (isMouseDown.value) {
+    return false
+  }
+
   if (item.name === hover.value) {
     return true
   }
@@ -159,15 +151,6 @@ function mousedown (e: MouseEvent) {
 
   startPosition.value.x = svgPosition.x
   startPosition.value.y = svgPosition.y
-
-  // const matrix = svgRef.value?.getScreenCTM()
-  // if (!matrix) {
-  //   return
-  // }
-  // startPosition.x = e.clientX - matrix?.e
-  // startPosition.y = e.clientY - matrix?.f
-//   startPosition.x = matrix?.e
-//   startPosition.y = matrix?.f
 }
 
 function convertScreenPosition (x: number, y: number) {
@@ -182,20 +165,20 @@ function convertScreenPosition (x: number, y: number) {
     svgPoint1.y = y
     const svgCoords = svgPoint1.matrixTransform(matrix.inverse())
 
-    return { x: svgCoords.x, y: svgCoords.y }
+    const x1 = svgCoords.x / tempZoom.value
+    const y1 = svgCoords.y / tempZoom.value
+
+    return { x: x1, y: y1 }
   }
 
   return { x: 0, y: 0 }
 }
 
-function setCirclePosition (x: number, y: number) {
+function setCirclePositions (x: number, y: number) {
   const matrix = svgRef.value?.getScreenCTM()
   if (!matrix) {
     return
   }
-
-  pointCircle1.value.x = x
-  pointCircle1.value.y = y
 
   const svgPoint1 = svgRef.value?.createSVGPoint()
   if (svgPoint1) {
@@ -203,72 +186,29 @@ function setCirclePosition (x: number, y: number) {
     svgPoint1.y = y
     const svgCoords = svgPoint1.matrixTransform(matrix.inverse())
 
-    pointCircle2.value.x = svgCoords.x
-    pointCircle2.value.y = svgCoords.y
+    pointCircle2.value.x = svgCoords.x / tempZoom.value
+    pointCircle2.value.y = svgCoords.y / tempZoom.value
   }
 }
 
 function mousemove (e: MouseEvent) {
-  setCirclePosition(e.clientX, e.clientY)
+  setCirclePositions(e.clientX, e.clientY)
 
   if (isMouseDown.value) {
     currentPosition.value.x += pointCircle2.value.x - startPosition.value.x
     currentPosition.value.y += pointCircle2.value.y - startPosition.value.y
   }
-
-  // const matrix = svgRef.value?.getScreenCTM()
-  // if (!matrix) {
-  //   return
-  // }
-
-  // pointCircle1.value.x = e.clientX
-  // pointCircle1.value.y = e.clientY
-
-  // const point = {}
-  // point.x = e.clientX - matrix?.e
-  // point.y = e.clientY - matrix?.f
-  // mousePosition.value = { x: e.clientX, y: e.clientY }
-  // //   mousePosition.value = point
-
-  // // Calculate the SVG coordinates
-  // const svgPoint = svgRef.value.createSVGPoint()
-  // svgPoint.x = e.clientX
-  // svgPoint.y = e.clientY
-  // const svgCoords = svgPoint.matrixTransform(matrix.inverse())
-
-  // if (!appendedCircle) {
-  //   // Append the circle to the SVG
-
-  //   appendedCircle = true
-  // }
-
-  // if (isMouseDown.value) {
-  //   currentPosition.value.x += point.x - startPosition.x
-  //   currentPosition.value.y += point.y - startPosition.y
-  // }
-
-  // /** ************** */
-
-  // const svgPoint2 = svgRef.value.createSVGPoint()
-  // svgPoint2.x = e.clientX
-  // svgPoint2.y = e.clientY
-  // const svgCoords2 = svgPoint2.matrixTransform(matrix.inverse())
-
-  // svgCoords2.x = svgCoords.x - (svgCoords.x * 0)
-  // svgCoords2.y = svgCoords.y - (svgCoords.y * 0)
-
-  // const newScreenCoords = svgCoords2.matrixTransform(matrix)
-
-//   console.log({ x: e.clientX, y: e.clientY }, { x: newScreenCoords.x, ny: newScreenCoords.y })
 }
 
 function wheelChanged (e: WheelEvent) {
-  setCirclePosition(e.clientX, e.clientY)
+  setCirclePositions(e.clientX, e.clientY)
 
-  tempZoom.value += 0.1
+  console.log(e)
+
+  tempZoom.value += (e.deltaY / 1000)
 
   nextTick(() => {
-    setCirclePosition(e.clientX, e.clientY)
+    setCirclePositions(e.clientX, e.clientY)
   })
 }
 
@@ -276,207 +216,71 @@ document.addEventListener('mouseup', () => {
   isMouseDown.value = false
 })
 
-// function wheel4 (e: WheelEvent) {
-//   e.preventDefault()
-//   e.stopPropagation()
-//   const oldZoom = tempZoom.value
-//   if (e.deltaY <= 0) {
-//     tempZoom.value += ZOOM_SPEED
-//   } else {
-//     tempZoom.value -= ZOOM_SPEED
-//   }
-//   tempZoom.value = Math.min(Math.max(Math.round(tempZoom.value), 1), 10)
-
-//   console.log(e)
-
-//   point2.value.x = e.clientX
-//   point2.value.y = e.clientY
-
-//   let point = svgRef.value?.createSVGPoint()
-//   if (!point) {
-//     return
-//   }
-
-//   const matrix = svgRef.value?.getScreenCTM()
-//   if (!matrix) {
-//     return
-//   }
-//   console.log(svgRef.value?.getScreenCTM())
-
-//   point.x = e.clientX - matrix?.e
-//   point.y = e.clientY - matrix?.f
-//   point = point.matrixTransform(svgRef.value.getCTM())
-//   console.log(point)
-
-//   point1.value = point
-
-//   const deltaNew = {
-//     x: (point.x * (tempZoom.value / oldZoom)) - point.x,
-//     y: (point.y * (tempZoom.value / oldZoom)) - point.y
-//   }
-
-//   currentPosition.value.x -= deltaNew.x
-//   currentPosition.value.y -= deltaNew.y
-
-//   emit('zoomchanged', tempZoom.value)
-// }
-
-// function wheelBetter (e: WheelEvent) {
-//   e.preventDefault()
-//   e.stopPropagation()
-
-//   const matrix = svgRef.value?.getScreenCTM()
-//   if (!matrix) {
-//     return
-//   }
-//   const svgPoint = svgRef.value.createSVGPoint()
-//   svgPoint.x = e.clientX
-//   svgPoint.y = e.clientY
-//   const svgCoords = svgPoint.matrixTransform(matrix.inverse())
-//   const oldZoom = tempZoom.value
-//   //   if (e.deltaY <= 0) {
-//   //     tempZoom.value += ZOOM_SPEED
-//   //   } else {
-//   //     tempZoom.value -= ZOOM_SPEED
-//   //   }
-//   //   tempZoom.value = Math.min(Math.max(tempZoom.value, 1), 10)
-
-//   //   const point = {}
-//   //   point.x = e.clientX - matrix?.e
-//   //   point.y = e.clientY - matrix?.f
-//   //   point.x = matrix?.e
-//   //   point.y = matrix?.f
-
-//   //   const deltaNew = {
-//   //     x: (point.x * (tempZoom.value / oldZoom)) - point.x,
-//   //     y: (point.y * (tempZoom.value / oldZoom)) - point.y
-//   //   }
-
-//   //   currentPosition.value.x -= deltaNew.x
-//   //   currentPosition.value.y -= deltaNew.y
-
-//   //   const deltaNew = svgRef.value.createSVGPoint()
-//   //   //   deltaNew.x = svgCoords.x
-//   //   //   deltaNew.y = svgCoords.y
-//   //   deltaNew.x = svgCoords.x - (svgCoords.x * (tempZoom.value / oldZoom))
-//   //   deltaNew.y = svgCoords.y - (svgCoords.y * (tempZoom.value / oldZoom))
-//   //   console.log({ x: deltaNew.x, y: deltaNew.y }, { x: svgCoords.x, ny: svgCoords.y })
-
-//   const svgPointAfterZoom = svgRef.value.createSVGPoint()
-//   svgPointAfterZoom.x = e.clientX
-//   svgPointAfterZoom.y = e.clientY
-//   const svgCoordsAfterZoom = svgPointAfterZoom.matrixTransform(matrix.inverse())
-
-//   const coordsBeforeZoom = svgPoint.matrixTransform(matrix)
-
-//   console.log({ x: e.clientX, y: e.clientY }, { x: coordsBeforeZoom.x, ny: coordsBeforeZoom.y })
-
-//   //   const newScreenCoords = deltaNew.matrixTransform(matrix)
-
-//   //   currentPosition.value.x = newScreenCoords.x
-//   //   currentPosition.value.y = newScreenCoords.y
-
-//   currentPosition.value.x += e.clientX - coordsBeforeZoom.x
-//   currentPosition.value.y += e.clientY - coordsBeforeZoom.y
-
-//   const deltaNew = svgRef.value.createSVGPoint()
-//   deltaNew.x = svgPointAfterZoom.x - svgPoint.x
-//   deltaNew.y = svgPointAfterZoom.y - svgPoint.y
-
-//   console.log(deltaNew)
-
-//   currentPosition.value.x += point.x - startPosition.x
-//   currentPosition.value.y += point.y - startPosition.y
-// }
-// function wheelBetter1 (e: WheelEvent) {
-//   e.preventDefault()
-//   e.stopPropagation()
-//   const oldZoom = tempZoom.value
-//   if (e.deltaY <= 0) {
-//     tempZoom.value += ZOOM_SPEED
-//   } else {
-//     tempZoom.value -= ZOOM_SPEED
-//   }
-//   tempZoom.value = Math.min(Math.max(tempZoom.value, 1), 10)
-
-//   const matrix = svgRef.value?.getScreenCTM()
-//   if (!matrix) {
-//     return
-//   }
-//   const point = {}
-//   point.x = e.clientX - matrix?.e
-//   point.y = e.clientY - matrix?.f
-//   point.x = matrix?.e
-//   point.y = matrix?.f
-
-//   const deltaNew = {
-//     x: (point.x * (tempZoom.value / oldZoom)) - point.x,
-//     y: (point.y * (tempZoom.value / oldZoom)) - point.y
-//   }
-
-//   currentPosition.value.x -= deltaNew.x
-//   currentPosition.value.y -= deltaNew.y
-
-// //   currentPosition.value.x += point.x - startPosition.x
-// //   currentPosition.value.y += point.y - startPosition.y
-// }
 </script>
 
 <template>
   <div
-    v-if="debugRef"
+    class="row"
+    style="height: 50px;"
   >
-    CP:{{ currentPosition }}<br>
-    SP:{{ startPosition }}<br>
-    VB:{{ calculateViewBox() }}<br>
-    Zoom: {{ props.zoom }} | {{ tempZoom }}<br>
-    {{ transformStyle }}<br>
-    WEP:{{ point2.x }}/{{ point2.y }}<br>
-    WEP:{{ point1?.x }}/{{ point1?.y }}<br>
-    Mouse:{{ mousePosition }}
+    <div class="col-3">
+      CP:{{ currentPosition.x }} / {{ currentPosition.y }}
+    </div>
+    <div class="col-3">
+      SP:{{ pointCircle2.x }} / {{ pointCircle2.y }}
+    </div>
+    <div class="col-2">
+      VB:{{ calculateViewBox() }}
+    </div>
+    <div class="col-2">
+      Zoom: {{ props.zoom }} | {{ tempZoom }}
+    </div>
+    <div class="col-2">
+      {{ transformStyle }}
+    </div>
   </div>
-  <!-- <div
-    ref="svgHomeRef"
-    style="width: 2000px; overflow: hidden;"
-  > -->
+
   <svg
-    id="SvgMap"
     ref="svgRef"
     version="1.0"
     xmlns="http://www.w3.org/2000/svg"
     :viewBox="calculateViewBox()"
-    style="background-color: brown; transform-origin: 0 0;"
-    :style="{transform: transformStyle}"
+    style="background-color: brown; border:2px solid #ddd;"
+
+    width="100%"
+    height="100%"
+    preserveAspectRatio="xMinYMin meet"
+
     @mousedown="mousedown"
     @mousemove="mousemove"
     @wheel.prevent="wheelChanged"
   >
+    <g :transform="transformStyle">
+      <template v-for="(item, index) in mapDataProvider.mapItems">
+        <polygon
+          v-if="!isSelected(item)"
+          :key="`polygon${index}`"
+          :points="createPolygon(item.points)"
+          vector-effect="non-scaling-stroke"
+          :stroke-width="`0.1vw`"
+          :stroke="getStrokeColor(item)"
+          :fill="getFillColor(item)"
+        />
+        <polygon
+          v-else
+          :key="`polygon-selected${index}`"
+          :points="createPolygon(item.points)"
+          :stroke-width="`0.5px`"
+          :stroke="getStrokeColor(item)"
+          :fill="getFillColor(item)"
+        />
+      </template>
 
-    <template v-for="(item, index) in mapDataProvider.mapItems">
-      <polygon
-        v-if="!isSelected(item)"
-        :key="`polygon${index}`"
-        :points="createPolygon(item.points)"
-        vector-effect="non-scaling-stroke"
-        :stroke-width="`0.1vw`"
-        :stroke="getStrokeColor(item)"
-        :fill="getFillColor(item)"
-      />
-      <polygon
-        v-else
-        :key="`polygon-selected${index}`"
-        :points="createPolygon(item.points)"
-        :stroke-width="`0.5px`"
-        :stroke="getStrokeColor(item)"
-        :fill="getFillColor(item)"
-      />
-    </template>
-
-    <g
-      v-for="(item, index) in mapDataProvider.mapItems"
-      :key="`group${index}`"
-    >
-      <!-- <text
+      <g
+        v-for="(item, index) in mapDataProvider.mapItems"
+        :key="`group${index}`"
+      >
+        <!-- <text
                 v-if="item.active || isSelected(item)"
                 :x="getCenterPoint(item.points).x"
                 :y="getCenterPoint(item.points).y"
@@ -487,56 +291,42 @@ document.addEventListener('mouseup', () => {
             >{{ item.name }}
             </text> -->
 
-      <text
-        v-if="isHover(item)"
-        :x="getCenterPoint(item.points).x"
-        :y="getCenterPoint(item.points).y"
-        dominant-baseline="middle"
-        text-anchor="middle"
-        font-size="1.5em"
-        fill="#000000"
-      >{{ item.name }}
-      </text>
+        <text
+          v-if="isHover(item)"
+          :x="getCenterPoint(item.points).x"
+          :y="getCenterPoint(item.points).y"
+          dominant-baseline="middle"
+          text-anchor="middle"
+          font-size="1.5em"
+          fill="#000000"
+        >{{ item.name }}
+        </text>
 
-      <polygon
-        :points="createPolygon(item.points)"
-        :stroke="'transparent'"
-        :fill="'transparent'"
-        @click="selectItem(item.name)"
-        @mouseover="mouseOver(item)"
-        @mouseout="mouseOut()"
+        <polygon
+          :points="createPolygon(item.points)"
+          :stroke="'transparent'"
+          :fill="'transparent'"
+          @click="selectItem(item.name)"
+          @mouseover="mouseOver(item)"
+          @mouseout="mouseOut()"
+        />
+      </g>
+
+      <circle
+        :r="34"
+        :cx="pointCircle2.x"
+        :cy="pointCircle2.y"
+        fill="black"
       />
+      <text
+        :x="pointCircle2.x"
+        :y="pointCircle2.y"
+        text-anchor="middle"
+        stroke="#fff"
+      >Correct</text>
     </g>
 
-    <circle
-      r="45"
-      :cx="pointCircle1.x"
-      :cy="pointCircle1.y"
-      fill="yellow"
-    />
-
-    <text
-      :x="pointCircle1.x"
-      :y="pointCircle1.y"
-      text-anchor="middle"
-      stroke="#000"
-    >Scrren</text>
-
-    <circle
-      r="34"
-      :cx="pointCircle2.x"
-      :cy="pointCircle2.y"
-      fill="black"
-    />
-    <text
-      :x="pointCircle2.x"
-      :y="pointCircle2.y"
-      text-anchor="middle"
-      stroke="#fff"
-    >Correct</text>
-
   </svg>
-  <!-- </div> -->
 </template>
 
 <style scoped>
