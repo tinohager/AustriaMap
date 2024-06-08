acbf6e09feac9f34e33741f550cd8d42bf872c5b
Tino Hager <tino.hager@nager.at>
2024-06-08 10:07:40 +0200

    cleanup provider

--- src/src/dataProviders/vorarlbergDataProvider.ts
+++ src/src/dataProviders/vorarlbergDataProvider.ts
@@ -292,49 +292,4 @@ const mapItems : MapItem[] = [
     points: [{ x: 508.8, y: 1616 }, { x: 403.2, y: 1534.4 }, { x: 456, y: 1486.4 }, { x: 523.2, y: 1457.6 }, { x: 619.19, y: 1438.4 }, { x: 681.59, y: 1491.2 }, { x: 791.99, y: 1438.4 }, { x: 719.99, y: 1548.8 }, { x: 561.59, y: 1572.8 }, { x: 508.79, y: 1616 }]
   }]
 
-// function roundTo (n : number, digits : number) {
-//   if (digits === undefined) {
-//     digits = 0
-//   }
-
-//   const multiplicator = Math.pow(10, digits)
-//   n = parseFloat((n * multiplicator).toFixed(11))
-//   const test = (Math.round(n) / multiplicator)
-//   return +(test.toFixed(digits))
-// }
-
-// const mapItems1 = mapItems.map(o => ({
-//   name: o
-//     .name,
-//   points: o
-//     .points.map(p => ({ x: roundTo((p.x - 30) * 20, 2), y: roundTo((p.y - 580) * 20, 2) }))
-// }))
-
-// const rasterSize = 40
-// const rectangleSize = 100
-
-// for (let x = 0; x < rasterSize; x++) {
-//   for (let y = 0; y < rasterSize; y++) {
-//     const calculatedX = rectangleSize * x
-//     const calculatedY = rectangleSize * y
-
-//     const x1 = calculatedX
-//     const x2 = calculatedX + rectangleSize
-//     const x3 = calculatedX + rectangleSize
-//     const x4 = calculatedX
-
-//     const y1 = calculatedY
-//     const y2 = calculatedY
-//     const y3 = calculatedY + rectangleSize
-//     const y4 = calculatedY + rectangleSize
-
-//     mapItems.push({
-
-//       name: `R${y + 1}/${x + 1}`,
-
-//       points: [{ x: x1, y: y1 }, { x: x2, y: y2 }, { x: x3, y: y3 }, { x: x4, y: y4 }]
-//     })
-//   }
-// }
-
 export const dataProvider : MapDataProvider = { mapItems }
